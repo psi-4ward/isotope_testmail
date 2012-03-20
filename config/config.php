471f0cadfe52b91d5efdfb1f556b2f9dc52d3fbc
Christoph Wiechert <wio@psitrax.de>
2012-03-20 22:36:13 +0100

    Fixed: Translation and copyright headers

--- config/config.php
+++ config/config.php
@@ -1,10 +1,31 @@
 <?php if(!defined('TL_ROOT')) die('You cannot access this file directly!');
 
 /**
- * @copyright 4ward.media 2012 <http://www.4wardmedia.de>
- * @author Christoph Wiechert <wio@psitrax.de>
- * @package Isotope
- * @licence LGPL
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Isotope eCommerce Workgroup 2011-2012
+ * @author     Christoph Wiechert <christoph.wiechert@4wardmedia.de>
+ * @author     Fred Bliss <fred.bliss@intelligentspark.com>
+ * @package	   Isotope
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
 
 $GLOBALS['ISO_MOD']['config']['iso_mail']['sendTestmail'] 		= array('IsotopeTestmail','generate');
--- languages/en/tl_iso_mail_content.php
+++ languages/en/tl_iso_mail_content.php
@@ -1,17 +1,38 @@
 <?php if(!defined('TL_ROOT')) die('You cannot access this file directly!');
 
 /**
- * @copyright 4ward.media 2012 <http://www.4wardmedia.de>
- * @author Christoph Wiechert <wio@psitrax.de>
- * @package Isotope
- * @licence LGPL
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program. If not, please visit the Free
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
+ *
+ * PHP version 5
+ * @copyright  Isotope eCommerce Workgroup 2011-2012
+ * @author     Christoph Wiechert <christoph.wiechert@4wardmedia.de>
+ * @author     Fred Bliss <fred.bliss@intelligentspark.com>
+ * @package	   Isotope
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
 
-$GLOBALS['TL_LANG']['tl_iso_mail_content']['sendTestmail_header']	= 'Test E-Mail verschicken';
-$GLOBALS['TL_LANG']['tl_iso_mail_content']['send']					= 'E-Mail verschicken';
+$GLOBALS['TL_LANG']['tl_iso_mail_content']['sendTestmail_header']	= 'Send a test e-mail';
+$GLOBALS['TL_LANG']['tl_iso_mail_content']['send']					= 'send the e-mail';
 
-$GLOBALS['TL_LANG']['tl_iso_mail_content']['sendTo']				= array('E-Mail Adresse','An diese E-Mail Adresse wird die Testmail geschickt.');
-$GLOBALS['TL_LANG']['tl_iso_mail_content']['orderId']				= array('Bestellung-ID','Die E-Mail wird mit den Daten dieser Bestellung befüllt.');
+$GLOBALS['TL_LANG']['tl_iso_mail_content']['sendTo']				= array('E-mail addresse','Test mails are being sent to this email address.');
+$GLOBALS['TL_LANG']['tl_iso_mail_content']['orderId']				= array('Order ID','This data of this order is used to fille the email.');
 
 $GLOBALS['TL_LANG']['tl_iso_mail_content']['orderNotFoundError']	= "The order ID %s was not found!";
 $GLOBALS['TL_LANG']['tl_iso_mail_content']['noOrderFoundError']		= "Theres no order to use for testdata! Please do some testordering ;)";
